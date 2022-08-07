43d46e30810c7b6933ca31d9afb587bbbe305e33
Tomás <tomas950923@gmail.com>
2022-08-06 20:08:26 -0500

    added files

---
+++ servertest_spawnregions.lua
@@ -0,0 +1,18 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "Blackwood", file = "media/maps/Blackwood/spawnpoints.lua" },
+		{ name = "BarcoAbandonado", file = "media/maps/BarcoAbandonado/spawnpoints.lua" },
+		{ name = "Factory", file = "media/maps/Factory/spawnpoints.lua" },
+		{ name = "MilitaryComplex", file = "media/maps/MilitaryComplex/spawnpoints.lua" },
+		{ name = "Riverside Gunstore", file = "media/maps/Riverside Gunstore/spawnpoints.lua" },
+		{ name = "Mansion", file = "media/maps/Mansion/spawnpoints.lua" },
+		{ name = "mechanicsplaces", file = "media/maps/mechanicsplaces/spawnpoints.lua" },
+		{ name = "tluisville", file = "media/maps/tluisville/spawnpoints.lua" },
+		{ name = "tmarch", file = "media/maps/tmarch/spawnpoints.lua" },
+		{ name = "tvalley", file = "media/maps/tvalley/spawnpoints.lua" },
+	}
+end
